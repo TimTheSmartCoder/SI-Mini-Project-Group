38e5118296ed3b4f34ec15cdd9f19ca1eee68aef
Tim Knudsen <timthesmartcoder@outlook.dk>
2018-03-01 09:09:01 +0100

    Moved Retailer.

---
+++ Retailer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Core.Application;
+
+namespace Retailer.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IApplication application = new RetailerClient("Retailer");
+
+            application.Start(args);
+
+            Console.ReadLine();
+
+            application.Stop();
+        }
+    }
+}
---
+++ Retailer/RetailerClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Application;
+using EasyNetQ;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Retailer.Client
+{
+    public class RetailerClient
+        : BaseApplication
+    {
+        private readonly IBus bus;
+
+        public RetailerClient(string name) 
+            : base(name)
+        {
+            this.bus = this.GetService<IBus>();
+        }
+
+        public override void Start(string[] args)
+        { }
+
+        public override void Stop()
+        {
+            // Dispose the IBus when application closes.
+            this.bus?.Dispose();
+        }
+
+        protected override void ConfigureServices(
+            IServiceCollection serviceCollection)
+        {
+            // Add singleton of IBus for EasyNetQ.
+            serviceCollection.AddSingleton<IBus>(
+                RabbitHutch.CreateBus($"host={this.configuration["RabbitMQ:Host"]}"));
+
+            serviceCollection.AddMediatR(typeof(RetailerClient));
+        }
+    }
+}
